9efab9468673612d0ca1a1dde92ba2916df2fe58
Joshua Poore <jpoore@arlis.umd.edu>
2022-09-30 23:21:05 -0400

    successfully adds custom filter to content.js script (custom.js doesn't work)

--- src/UserALEWebExtension/custom.js
+++ src/UserALEWebExtension/custom.js
@@ -1,8 +1,8 @@
-window.userale.filter(function (log) {
+/*filter(function (log) {
     var type_array = ['mouseup', 'mouseover', 'mousedown', 'keydown', 'dblclick', 'blur', 'focus', 'input', 'wheel'];
     var logType_array = ['interval'];
     return !type_array.includes(log.type) && !logType_array.includes(log.logType);
   });
-
+*/
 
   
