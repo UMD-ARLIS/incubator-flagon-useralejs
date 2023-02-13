cd5b58197158e9636a0c5d3e8a85510913a2b9eb
Jason Young <jason_y54@protonmail.com>
2023-02-13 13:36:35 -0800

    Added build with Kafka

--- build/UserALEWebExtension/content.js
+++ build/UserALEWebExtension/content.js
@@ -959,12 +959,21 @@ function sendLogs(logs, config, retries) {
   var req = new XMLHttpRequest();
 
   // @todo setRequestHeader for Auth
-  var data = JSON.stringify(logs);
+  var data = JSON.stringify({
+    records: logs.map(function (log) {
+      return {
+        key: log['sessionID'],
+        value: log
+      };
+    })
+  });
+  console.log(data);
   req.open('POST', config.url);
   if (config.authHeader) {
     req.setRequestHeader('Authorization', config.authHeader);
   }
-  req.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+  req.setRequestHeader('Content-type', 'application/vnd.kafka.json.v2+json');
+  req.setRequestHeader('Accept', 'application/vnd.kafka.v2+json');
   req.onreadystatechange = function () {
     if (req.readyState === 4 && req.status !== 200) {
       if (retries > 0) {
